1b7d6a12ca6eee9c830e775b4b786838bae5f754
Blatko1 <kings.of.funny.gemplays@gmail.com>
2021-06-16 11:13:39 +0200

    Added the project.

---
+++ src/hm_tools/mod.rs
@@ -0,0 +1,56 @@
+pub mod art;
+
+pub const WORDS: [&'static str; 9] = [
+    "house",
+    "turtle",
+    "building",
+    "pancake",
+    "catnip",
+    "keyboard",
+    "rust programming language",
+    "made by Leon",
+    "copyright included",
+];
+
+pub mod tools{
+    use crate::hm_tools::WORDS;
+    use rand::prelude::*;
+
+    pub fn random_word() -> String {
+        let mut rng = rand::thread_rng();
+        String::from(WORDS[rng.gen_range(0..9)])
+    }
+
+    pub fn encrypt_word(word: &String) -> String {
+        let size = word.chars().count();
+        let mut result = String::new();
+
+        for l in word.chars(){
+            if l as i32 >= 97 && l as i32 <= 122 {
+                result.push('_');
+                result.push(' ');
+            }
+            if l as i32 == 32 {
+                result.push(' ');
+            }
+        };
+
+        result
+        
+    }
+
+    pub fn remove_duplicates(list: &mut Vec<char>) {
+        list.sort();
+        list.dedup();
+    }
+
+    pub fn decrypt_encrypted(guess: char, encr: &mut String, word: String) {
+        let mut positions: Vec<i32> = Vec::new();
+        for c in word.chars() {
+            if(c == char){
+                positions.push(value: T)
+            }
+        }
+    }
+
+}
---
+++ src/main.rs
@@ -0,0 +1,78 @@
+mod hm_tools;
+
+use hm_tools::{tools, art};
+use std::io;
+
+fn main() {
+    std::process::Command::new("clear").status().unwrap();
+    println!("Welcome to Hangman Offical Terminal Game 1978 Copyright, all Right Reserved");
+
+    start();
+
+}
+
+fn start() {
+
+    let mut mistakes = 0;
+    let secret = tools::random_word();
+    let mut temp: Vec<char> = secret.chars().collect();
+    tools::remove_duplicates(&mut temp);
+    let mut encrypted = tools::encrypt_word(&secret);
+    let mut guess: char;
+
+    loop{
+        println!("{}\n", art::SPRITES[mistakes]);
+        println!("{}\n", encrypted);
+        println!("{}\n", secret);
+        println!("{:?}\n", temp);
+        
+        guess = get_input();
+        if is_guessed(guess, &mut temp) {
+            println!("There!");
+        }
+
+        std::process::Command::new("clear").status().unwrap();
+
+        if temp.is_empty() {
+            println!("______________________You Win!________________________");
+            break;
+        }
+
+    };
+
+}
+
+fn is_guessed(guess: char, list: &mut Vec<char>) -> bool{
+
+    if list.contains(&guess){
+        list.remove(list.iter().position(|&p| p == guess).unwrap());
+
+        return true;
+    }
+
+    false
+}
+
+fn get_input() -> char {
+
+    let mut input = String::new();
+    
+    loop {
+        input = String::new();
+        println!("Try guessing the letter!");
+        match io::stdin().read_line(&mut input) {
+            Ok(_n) => {
+                if input.trim().chars().count() == 1 {
+                    return input.trim().chars().nth(0).unwrap();
+                }
+                println!("Enter only one letter!");
+                continue;
+            },
+            Err(_err) => {
+                println!("Failed reading the input!");
+                continue;
+            },
+        }
+    };
+
+}
